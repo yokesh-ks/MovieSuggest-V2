5eea3a33e4ca3c02a76a0296138aab9314c85c4e
yokesh-ks <ksyokesh98@gmail.com>
2022-04-02 21:22:08 +0530

    api config and fetch method added

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,26 +1,38 @@
 import React, { useState, useEffect } from "react";
-import { allData } from "../utilis/api";
 import { CardsContainer } from "../components/CardsSection";
 import Header from "../components/Header";
 import { isWeb } from "../utilis/screenLayouts";
-
+import { allMovies } from "../api";
+import { Layout } from "../components/Layout";
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { innerWidth: width } = window;
-  console.log(movies)
+  console.log(movies);
 
   useEffect(() => {
     (async () => {
-      const data = await allData();
-      setMovies(data.allMovies);
+      const data = await allMovies();
+      setMovies(data.data);
+      setLoading(false);
     })();
   }, []);
 
   return (
     <>
       <Header />
-      <CardsContainer data={movies} isWeb={isWeb(width)} />
+      <Layout>
+        <div>
+          {loading ? (
+            <div style={{ paddingTop: 300 }}>
+              <h1>Loading...</h1>
+            </div>
+          ) : (
+            <CardsContainer data={movies} isWeb={isWeb(width)} />
+          )}
+        </div>
+      </Layout>
     </>
   );
 };
